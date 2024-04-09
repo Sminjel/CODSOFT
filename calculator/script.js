42b21802aaf007ffd1f67dc02bdc3c02ef275195
Simangele <72740114+Sminjel@users.noreply.github.com>
2024-04-09 21:58:01 +0200

    initial commit

---
+++ calculator/script.js
@@ -0,0 +1,21 @@
+let displayValue = '';
+
+function appendToDisplay(value) {
+  displayValue += value;
+  document.getElementById('display').innerText = displayValue;
+}
+
+function clearDisplay() {
+  displayValue = '';
+  document.getElementById('display').innerText = '0';
+}
+
+function calculate() {
+  try {
+    const result = eval(displayValue);
+    document.getElementById('display').innerText = result;
+    displayValue = result.toString();
+  } catch (error) {
+    document.getElementById('display').innerText = 'Error';
+  }
+}
